1294cfe80fa2c278ab11ee8ed7b1618d3a224204
unknown <bihackquai@gmail.com>
2021-12-23 15:21:15 +0700

    update moi ngay 23

--- configs/database.js
+++ configs/database.js
@@ -7,4 +7,5 @@ module.exports = {
     users: 'users',
     category: 'category',
     article: 'article',
+
 };
--- routes/frontend/home.js
+++ routes/frontend/home.js
@@ -2,22 +2,39 @@ var express = require('express');
 var router = express.Router();
 const folderView = ('blog/pages/home/');
 const ArticleModel = require('../../models/article'); // patch models
+const CategoryModel = require('../../models/category'); // patch models
+const SocialItem = require('../../models/items'); // patch models
 /* GET home admin page. */
-router.get('/', async function(req, res, next) {
-  let itemsTrend = [];
+router.get('/', async function(req, res, next) { let itemsTrend = [];
   let itemsNew = []; 
-  console.log()
+  let itemsCategory = [];
+  let itemsNewsHomePage = [];
+  let itemSocial = [];
    await ArticleModel.listItemFrontend(null,{task:'top-post'}).then((items)=>{
     itemsTrend = items ; 
     });
     await ArticleModel.listItemFrontend(null,{task:'list-news'}).then((items)=>{
       itemsNew = items ; 
       });
+    await CategoryModel.listItemFrontend(null,{task:'list-category'}).then((items)=>{
+      itemsCategory = items ; 
+    });
+    await ArticleModel.listItemFrontend(null,{task:'list-news-category-homepage'}).then((items)=>{
+      itemsNewsHomePage = items ; 
+    });
+    await SocialItem.listItemSocial(null,{task:'list-social'}).then((items)=>{
+      itemSocial = items ; 
+    });
+              
     res.render(`${folderView}index`,{
       top_post:true,
       itemsTrend,
-      itemsNew
+      itemsNew,
+      itemsCategory,
+      itemsNewsHomePage,
+      itemSocial
   });
+ 
 });
 
 module.exports = router;
